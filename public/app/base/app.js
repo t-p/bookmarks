9d9b1bc9461f66233c776933bb6c12524f07e71c
Thomas Pfeiffer <email@pfeiffer.pw>
2013-12-19 22:58:54 +0100

    first steps with angular

--- public/app/base/app.js
+++ public/app/base/app.js
@@ -4,11 +4,46 @@
 //    return $resource("/bookmarks/:id", {id:"@id"});
 //  });
 //
+//  app.controller('TextController',
+//    function($scope) {
+//    var someText = {};
+//    someText.message = 'You have started your journey.';
+//    $scope.someText = someText;
 //})(
 //  angular.module("app_base", ["ngResource"])
 //);
-var myAppModule = angular.module('app_base', []);
 
+var myAppModule = angular.module('app_base', ['ngResource']);
+
+myAppModule.factory("Bookmark", ['$resource',
+  function($resource) {
+    return $resource("/bookmarks/:id", {id:"@id"});
+}]);
+myAppModule.factory("bookmarks", function(Bookmark) {
+  return Bookmark.query();
+});
+myAppModule.factory("saveBookmark", function(bookmarks, state) {
+  return function(bookmark) {
+    if (!bookmark.id) {
+      bookmarks.push(bookmark);
+    }
+    bookmark.$save();
+    state.clearForm();
+  };
+});
+myAppModule.service("state", function(Bookmark) {
+  this.formBookmark = {bookmark:new Bookmark()};
+  this.clearForm = function() {
+    this.formBookmark.bookmark = new Bookmark();
+  };
+});
+
+myAppModule.controller("BookmarkFormController",
+  function($scope, state, bookmarks, saveBookmark) {
+    $scope.formBookmark = state.formBookmark;
+    $scope.saveBookmark = saveBookmark;
+    $scope.clearForm = state.clearForm;
+});
 myAppModule.controller('TextController',
   function($scope) {
   var someText = {};
